3f2e51e26ffac0fe1ce47f631ac977189f946c00
Paul Rozehnal <p.rozehnal@gmail.com>
2018-11-03 17:19:04 -0400

    Update Problem #1.py

--- Problem #1.py
+++ Problem #1.py
@@ -5,3 +5,23 @@
 >>> z
 233168
 #correct.
+#hangups included 1) trying an if elif thread ('or' is much more suitable, 
+	#if/elif stops the loop after an if has been proved true for the first time.
+		#2) using a for loop (inherently iterative) is much cleaner than a while loop. though i could/will try now.
+>>> z=0
+>>> x=0
+>>> i = int(input("enter the range you want"))
+enter the range you want1000
+
+>>> type(i)
+<class 'int'> #showing it is an integer, not a str.
+
+>>> while x<1000:
+	x=x+1
+	if x%5==0 or x%3==0:
+		z=z+x-i
+>>> z
+-232832
+##obviously wrong, because the 1000 is subtracted on each iteration.
+##thinking i must need a user-defined function which give me the oppurtunity to subtract the int(input('range:') only once)
+#ugly.
