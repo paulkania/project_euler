328833b9b51dd8fde7f0888115539e3fcf4ebc30
Paul Rozehnal <p.rozehnal@gmail.com>
2018-11-03 17:43:16 -0400

    Update Problem #1.py

--- Problem #1.py
+++ Problem #1.py
@@ -3,8 +3,7 @@
 	if x%5==0 or x%3==0:
 		z=z+x
 >>> z
-233168
-#correct.
+233168 #correct.
 #hangups included 1) trying an if elif thread ('or' is much more suitable, 
 	#if/elif stops the loop after an if has been proved true for the first time.
 		#2) using a for loop (inherently iterative) is much cleaner than a while loop. though i could/will try now.
@@ -16,7 +15,7 @@ enter the range you want1000
 >>> type(i)
 <class 'int'> #showing it is an integer, not a str.
 
->>> while x<1000:
+>>> while x<i:
 	x=x+1
 	if x%5==0 or x%3==0:
 		z=z+x-i
@@ -25,3 +24,25 @@ enter the range you want1000
 ##obviously wrong, because the 1000 is subtracted on each iteration.
 ##thinking i must need a user-defined function which give me the oppurtunity to subtract the int(input('range:') only once)
 #ugly.
+#edit1 idea didn't work (add 'ans' variable which is ans=z-i because i created the 'ans' variable before the loop, so z 
+#was defined as 0, not 234168. and adding a ans
+
+#edit2 I got it, but it's not super pretty, pretty patchy in fact. the following.
+VVV
+>>> x=0
+>>> z=0
+>>> ans=0
+>>> i = int(input("enter the range you want:  "))
+enter the range you want:  1000
+while x<i:
+	x=x+1
+	if x%5==0 or x%3==0:
+		z=z+x
+	ans=z-i
+>>> ans
+233168
+>>> z
+234168
+>>> i
+1000
+VVV
