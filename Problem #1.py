06b6ead1599cb19e9112cad449bb878db89d712d
Paul Rozehnal <p.rozehnal@gmail.com>
2018-11-03 16:56:02 -0400

    Update Problem #1.py

--- Problem #1.py
+++ Problem #1.py
@@ -1,15 +1,7 @@
-Python 3.6.3 (v3.6.3:2c5fed86e0, Oct  3 2017, 00:32:08) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
-
 >>> z=0
 >>> for x in range(1000):
 	if x%5==0 or x%3==0:
 		z=z+x
-
-		
 >>> z
 233168
->>> 
+#correct.
